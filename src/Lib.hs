c976ac73e5ae5e8b10cb9d392056e7ee3396a9e4
MatiasRizzatoBusta <matias.rizzato@gmail.com>
2020-06-10 14:27:34 -0300

    pto 2 parte 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,16 +3,17 @@ import Text.Show.Functions
 laVerdad = True
 
 --------------------------------------------------- Parte 1 ---------------------------------------------------
+--------------------------------------------------- Punto 1 ---------------------------------------------------
 data Persona = UnaPersona{
     edad :: Int,
     items :: Items,
-    exp :: Int
+    xp :: Int
 }deriving (Show,Eq)
 
 type Items = [String]
 type Criterio = [String]
 
-data Criatura = UnSiempreDetras{ criterios :: Criterio}
+data Criatura = UnSiempreDetras{criterios :: Criterio}
                |UnosGnomos{cantidad:: Int,criterios :: Criterio}
                |UnFantasma{categoria :: Int,criterios :: Criterio}deriving (Show)
 
@@ -26,3 +27,17 @@ nivelPeligrosidad :: Criatura->Int
 nivelPeligrosidad (UnSiempreDetras _) = 0
 nivelPeligrosidad (UnosGnomos cantidad _) = 2^cantidad
 nivelPeligrosidad (UnFantasma categoria  _) = 20* categoria
+
+--------------------------------------------------- Punto 2 ---------------------------------------------------
+type Enfrentamiento = Persona->Criatura->Persona
+enfrentamiento :: Enfrentamiento
+enfrentamiento persona criatura |tieneElementoNecesario (items persona) criatura = ganaExp persona (nivelPeligrosidad criatura)
+                                |otherwise = ganaExp persona 1
+
+tieneElementoNecesario :: Items->Criatura->Bool -- uso recursividad para no armar otra funcion para evitar usar otra funcion 
+tieneElementoNecesario [] _ = False
+tieneElementoNecesario (x:xs) criatura = any ( == x ) (criterios criatura) || tieneElementoNecesario xs criatura
+-- si el primer item no cumple toma el segundo y asi hasta que se acaba la lista. si esta vacia es porque no lo tiene
+
+ganaExp :: Persona->Int->Persona
+ganaExp persona xpGanada = persona{xp = (xp persona) + xpGanada}
