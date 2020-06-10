f240fb169b86047393ede5dedbb82088033cd924
MatiasRizzatoBusta <matias.rizzato@gmail.com>
2020-06-10 15:25:12 -0300

    pto 3

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,40 +11,44 @@ data Persona = UnaPersona{
 }deriving (Show,Eq)
 
 type Items = [String]
-type Criterio = [String]
+type Criterio = Persona->Bool
 
-data Criatura = UnSiempreDetras{criterios :: Criterio} -- los pienso separado pq cada criatura tiene consideraciones distintas
-               |UnosGnomos{cantidad:: Int,criterios :: Criterio}
-               |UnFantasma{categoria :: Int,criterios :: Criterio}deriving (Show)
+data Criatura = UnaCriatura{
+    peligrosidad :: Int,
+    criterio :: Criterio
+}deriving (Show)
 
 ---------------------------------- Ejemplos criaturas ----------------------------------
+siempreDetras :: Criatura
+siempreDetras = UnaCriatura 0 puedeDeshacerseSiempreDetras -- el 0 es la peligrosidad que le paso a la funcion nivelPeligrosidad
 
-siempreDetras = UnSiempreDetras [] -- el 0 es la peligrosidad que le paso a la funcion nivelPeligrosidad
-grupoDeGnomos = UnosGnomos 20 ["soplador de hojas"]
-fantasma = UnFantasma 7 ["esparcir restos en el mar"]
+gnomos :: Int->Criatura
+gnomos cantidad = UnaCriatura (2^cantidad) puedeDeshacerseGnomos
 
-nivelPeligrosidad :: Criatura->Int --uso pattern matching para los distintos tipos de criatura
-nivelPeligrosidad (UnSiempreDetras _) = 0
-nivelPeligrosidad (UnosGnomos 1 _) = 0 -- si esta solo es inofensivo
-nivelPeligrosidad (UnosGnomos cantidad _) = 2^cantidad
-nivelPeligrosidad (UnFantasma categoria  _) = 20 * categoria
+fantasma :: Int->(Persona->Bool)->Criatura
+fantasma poder asuntoPendiente = UnaCriatura (2*poder) (puedeDeshacerseFantasma asuntoPendiente)
 --La desventaja esta en que si en el futuro agrego mas criaturas tengo que agregar un data nuevo y las nuevas condiciones a esta
 --funcion para que el programa ande correctamente.
 
+puedeDeshacerseSiempreDetras :: Criterio
+puedeDeshacerseSiempreDetras _ = False
+puedeDeshacerseGnomos :: Criterio
+puedeDeshacerseGnomos persona = elem "soplador de hojas" (items persona)
+puedeDeshacerseFantasma :: (Persona->Bool)->Criterio
+puedeDeshacerseFantasma asuntoPendiente persona = asuntoPendiente persona
+
 --------------------------------------------------- Punto 2 ---------------------------------------------------
 enfrentamiento :: Persona->Criatura->Persona
-enfrentamiento persona criatura |tieneElementoNecesario (items persona) criatura = ganaExp persona (nivelPeligrosidad criatura)
+enfrentamiento persona criatura |tieneElementoNecesario persona criatura = ganaExp persona (peligrosidad criatura)
                                 |otherwise = ganaExp persona 1
 
-tieneElementoNecesario :: Items->Criatura->Bool -- uso recursividad para no armar otra funcion para evitar usar otra funcion 
-tieneElementoNecesario [] _ = False
-tieneElementoNecesario (x:xs) criatura = any ( == x ) (criterios criatura) || tieneElementoNecesario xs criatura
--- si el primer item no cumple toma el segundo y asi hasta que se acaba la lista. si esta vacia es porque no lo tiene
+tieneElementoNecesario :: Persona->Criatura->Bool
+tieneElementoNecesario persona criatura = (criterio criatura) persona  
 
 ganaExp :: Persona->Int->Persona
 ganaExp persona xpGanada = persona{xp = (xp persona) + xpGanada}
 --------------------------------------------------- Punto 3 ---------------------------------------------------
-grupoDeCriaturas = [siempreDetras,grupoDeGnomos,fantasma]
+grupoDeCriaturas = [siempreDetras,gnomos 10,fantasma 3 ((<10).edad),((elem "disfraz de oveja").items)]
 
 enfrentamientoSucesivo :: Persona->[Criatura]->Int
 enfrentamientoSucesivo persona listaCriaturas = (diferenciaXp persona.foldl enfrentamiento persona) listaCriaturas
