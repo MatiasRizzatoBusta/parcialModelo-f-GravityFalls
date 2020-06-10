d15aa9d26c10cf6cca43c08578a15da3ca025656
MatiasRizzatoBusta <matias.rizzato@gmail.com>
2020-06-10 14:39:52 -0300

    pto 3 parte 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,7 +13,7 @@ data Persona = UnaPersona{
 type Items = [String]
 type Criterio = [String]
 
-data Criatura = UnSiempreDetras{criterios :: Criterio}
+data Criatura = UnSiempreDetras{criterios :: Criterio} -- los pienso separado pq cada criatura tiene consideraciones distintas
                |UnosGnomos{cantidad:: Int,criterios :: Criterio}
                |UnFantasma{categoria :: Int,criterios :: Criterio}deriving (Show)
 
@@ -21,16 +21,18 @@ data Criatura = UnSiempreDetras{criterios :: Criterio}
 
 siempreDetras = UnSiempreDetras [] -- el 0 es la peligrosidad que le paso a la funcion nivelPeligrosidad
 grupoDeGnomos = UnosGnomos 20 ["soplador de hojas"]
-fantasma = UnFantasma 
+fantasma = UnFantasma 7 ["esparcir restos en el mar"]
 
-nivelPeligrosidad :: Criatura->Int
+nivelPeligrosidad :: Criatura->Int --uso pattern matching para los distintos tipos de criatura
 nivelPeligrosidad (UnSiempreDetras _) = 0
+nivelPeligrosidad (UnosGnomos 1 _) = 0 -- si esta solo es inofensivo
 nivelPeligrosidad (UnosGnomos cantidad _) = 2^cantidad
-nivelPeligrosidad (UnFantasma categoria  _) = 20* categoria
+nivelPeligrosidad (UnFantasma categoria  _) = 20 * categoria
+--La desventaja esta en que si en el futuro agrego mas criaturas tengo que agregar un data nuevo y las nuevas condiciones a esta
+--funcion para que el programa ande correctamente.
 
 --------------------------------------------------- Punto 2 ---------------------------------------------------
-type Enfrentamiento = Persona->Criatura->Persona
-enfrentamiento :: Enfrentamiento
+enfrentamiento :: Persona->Criatura->Persona
 enfrentamiento persona criatura |tieneElementoNecesario (items persona) criatura = ganaExp persona (nivelPeligrosidad criatura)
                                 |otherwise = ganaExp persona 1
 
@@ -41,3 +43,11 @@ tieneElementoNecesario (x:xs) criatura = any ( == x ) (criterios criatura) || ti
 
 ganaExp :: Persona->Int->Persona
 ganaExp persona xpGanada = persona{xp = (xp persona) + xpGanada}
+--------------------------------------------------- Punto 3 ---------------------------------------------------
+grupoDeCriaturas = [siempreDetras,grupoDeGnomos,fantasma]
+
+enfrentamientoSucesivo :: Persona->[Criatura]->Int
+enfrentamientoSucesivo persona listaCriaturas = (diferenciaXp persona.foldl enfrentamiento persona) listaCriaturas
+-- le aplico la funcion enfrentamiento y persona a cada criatura y al resultado le calculo la diferencia con el del principio
+diferenciaXp :: Persona->Persona->Int
+diferenciaXp persona personaFinal = (xp personaFinal) - (xp persona)
