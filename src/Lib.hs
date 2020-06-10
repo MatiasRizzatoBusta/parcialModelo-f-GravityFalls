1922986aa104bad51dd2916792d547b95a06b9b0
MatiasRizzatoBusta <matias.rizzato@gmail.com>
2020-06-10 14:16:26 -0300

    pto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,28 @@
 module Lib where
 import Text.Show.Functions
-
 laVerdad = True
+
+--------------------------------------------------- Parte 1 ---------------------------------------------------
+data Persona = UnaPersona{
+    edad :: Int,
+    items :: Items,
+    exp :: Int
+}deriving (Show,Eq)
+
+type Items = [String]
+type Criterio = [String]
+
+data Criatura = UnSiempreDetras{ criterios :: Criterio}
+               |UnosGnomos{cantidad:: Int,criterios :: Criterio}
+               |UnFantasma{categoria :: Int,criterios :: Criterio}deriving (Show)
+
+---------------------------------- Ejemplos criaturas ----------------------------------
+
+siempreDetras = UnSiempreDetras [] -- el 0 es la peligrosidad que le paso a la funcion nivelPeligrosidad
+grupoDeGnomos = UnosGnomos 20 ["soplador de hojas"]
+fantasma = UnFantasma 
+
+nivelPeligrosidad :: Criatura->Int
+nivelPeligrosidad (UnSiempreDetras _) = 0
+nivelPeligrosidad (UnosGnomos cantidad _) = 2^cantidad
+nivelPeligrosidad (UnFantasma categoria  _) = 20* categoria
